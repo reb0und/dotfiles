5da43c36400876fbd5578df37332df801020c0be
reb0und <me@rebound.sh>
2025-07-23 09:30:45 -0700

    feat: add transparency nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1010,6 +1010,9 @@ require("lazy").setup({
 	  },
 	},
 	{
+		"xiyaowong/transparent.nvim",
+	},
+	{
 		"nvimtools/none-ls.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = function()
@@ -1137,8 +1140,3 @@ lspconfig.pyright.setup({
 vim.keymap.set("n", "<C-t>", "<Cmd>Neotree toggle<CR>", { desc = "Toggle NeoTree file explorer" })
 vim.keymap.set("n", "<C-y>", "<Cmd>Telescope find_files<CR>", { desc = "Find files fTelescope" })
 vim.keymap.set("n", "<C-g>", "<Cmd>Telescope live_grep<CR>", { desc = "Live grep using Telescope" })
-
-vim.api.nvim_set_hl(0, "Normal",       { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat",  { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder",  { bg = "none" })
-vim.api.nvim_set_hl(0, "NonText",      { bg = "none" })
