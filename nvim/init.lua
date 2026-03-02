56a90874e821962402aebf5c4c3e2c432d691901
reb0und <me@rebound.sh>
2026-03-02 09:12:53 -0500

    chore: rm wt

--- nvim/init.lua
+++ nvim/init.lua
@@ -170,7 +170,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {desc='Line diagnostics'})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Line diagnostics" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -673,11 +673,22 @@ require("lazy").setup({
 			end
 
 			local exepath = vim.fn.exepath
-			vim.lsp.config.lua_ls   = vim.tbl_deep_extend("force", { cmd = { exepath("lua-language-server") } }, vim.lsp.config.lua_ls or {})
-			vim.lsp.config.pyright  = vim.tbl_deep_extend("force", { cmd = { exepath("pyright-langserver"), "--stdio" } }, vim.lsp.config.pyright or {})
-			vim.lsp.config.gleam    = vim.tbl_deep_extend("force", { cmd = { exepath("gleam"), "lsp" } }, vim.lsp.config.gleam or {})
-			vim.lsp.config.clangd   = vim.tbl_deep_extend("force", { cmd = { exepath("clangd"), "--offset-encoding=utf-8" } }, vim.lsp.config.clangd or {})
-			vim.lsp.config.gopls    = vim.tbl_deep_extend("force", { cmd = { exepath("gopls") } }, vim.lsp.config.gopls or {})
+			vim.lsp.config.lua_ls =
+				vim.tbl_deep_extend("force", { cmd = { exepath("lua-language-server") } }, vim.lsp.config.lua_ls or {})
+			vim.lsp.config.pyright = vim.tbl_deep_extend(
+				"force",
+				{ cmd = { exepath("pyright-langserver"), "--stdio" } },
+				vim.lsp.config.pyright or {}
+			)
+			vim.lsp.config.gleam =
+				vim.tbl_deep_extend("force", { cmd = { exepath("gleam"), "lsp" } }, vim.lsp.config.gleam or {})
+			vim.lsp.config.clangd = vim.tbl_deep_extend(
+				"force",
+				{ cmd = { exepath("clangd"), "--offset-encoding=utf-8" } },
+				vim.lsp.config.clangd or {}
+			)
+			vim.lsp.config.gopls =
+				vim.tbl_deep_extend("force", { cmd = { exepath("gopls") } }, vim.lsp.config.gopls or {})
 
 			require("mason-tool-installer").setup({
 				ensure_installed = {
@@ -993,7 +1004,6 @@ require("lazy").setup({
 		cmd = "VimBeGood",
 		config = function() end,
 	},
-	{ "wakatime/vim-wakatime", lazy = false },
 	{
 		"folke/flash.nvim",
 		event = "VeryLazy",
@@ -1095,34 +1105,34 @@ require("lazy").setup({
 local cmp_caps = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 vim.lsp.config.gopls = vim.tbl_deep_extend("force", {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod" },
-  root_dir = vim.fs.root(0, { "go.work", "go.mod", ".git" }),
-  capabilities = cmp_caps,
-  settings = {
-    gopls = {
-      analyses = { unusedparams = true },
-      staticcheck = true,
-    },
-  },
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod" },
+	root_dir = vim.fs.root(0, { "go.work", "go.mod", ".git" }),
+	capabilities = cmp_caps,
+	settings = {
+		gopls = {
+			analyses = { unusedparams = true },
+			staticcheck = true,
+		},
+	},
 }, vim.lsp.config.gopls or {})
 
 vim.lsp.config.gleam = vim.tbl_deep_extend("force", {}, vim.lsp.config.gleam or {})
 
 vim.lsp.config.clangd = vim.tbl_deep_extend("force", {
-  cmd = { "clangd", "--offset-encoding=utf-8" },
-  init_options = {
-    usePlaceholders = true,
-    completeUnimported = true,
-    clangdFileStatus = true,
-  },
-  capabilities = cmp_caps,
+	cmd = { "clangd", "--offset-encoding=utf-8" },
+	init_options = {
+		usePlaceholders = true,
+		completeUnimported = true,
+		clangdFileStatus = true,
+	},
+	capabilities = cmp_caps,
 }, vim.lsp.config.clangd or {})
 
 vim.lsp.config.pyright = vim.tbl_deep_extend("force", {
-  settings = {
-    python = { analysis = { typeCheckingMode = "basic" } },
-  },
+	settings = {
+		python = { analysis = { typeCheckingMode = "basic" } },
+	},
 }, vim.lsp.config.pyright or {})
 
 pcall(vim.lsp.enable, "lua_ls")
