7d79272b13fb432287a2ab0e5d98bf2492d2cbf5
reb0und <me@rebound.sh>
2026-09-16 17:11:53 -0400

    fix(nvim): treesitter cfg

--- nvim/init.lua
+++ nvim/init.lua
@@ -561,7 +561,7 @@ require("lazy").setup({
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					if client and client:supports_method("textDocument/documentHighlight") then
 						local highlight_augroup =
 							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
@@ -589,7 +589,7 @@ require("lazy").setup({
 					-- code, if the language server you are using supports them
 					--
 					-- This may be unwanted, since they displace some of your code
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					if client and client:supports_method("textDocument/inlayHint") then
 						map("<leader>th", function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
 						end, "[T]oggle Inlay [H]ints")
@@ -929,33 +929,39 @@ require("lazy").setup({
 	},
 	{
 		"nvim-treesitter/nvim-treesitter",
-		branch = "main",
+		branch = "master",
 		lazy = false,
 		build = ":TSUpdate",
-		opts = {},
 		config = function()
-			local treesitter = require("nvim-treesitter")
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"c",
+					"diff",
+					"html",
+					"lua",
+					"luadoc",
+					"markdown",
+					"markdown_inline",
+					"query",
+					"vim",
+					"vimdoc",
+					"python",
+					"go",
+				},
 
-			treesitter.install({
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"python",
-				"go",
-			})
+				sync_install = false,
+				auto_install = true,
+				ignore_install = {},
+				modules = {},
 
-			vim.api.nvim_create_autocmd("FileType", {
-				callback = function(args)
-					pcall(vim.treesitter.start, args.buf)
-				end,
+				highlight = {
+					enable = true,
+				},
+
+				indent = {
+					enable = true,
+				},
 			})
 		end,
 	},
